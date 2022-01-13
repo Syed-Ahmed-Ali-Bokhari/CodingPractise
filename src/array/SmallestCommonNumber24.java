84f040d7a5aa5f3c337b7ba4fc792250d74f6913
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-01-13 14:33:52 +0500

    update

---
+++ src/array/SmallestCommonNumber24.java
@@ -0,0 +1,19 @@
+package array;
+
+public class SmallestCommonNumber24 {
+
+	public static int commonNumber(int[] array1, int[] array2, int[] array3) {
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+		int[] array1 = new int[] { 6, 21, 77, 88 };
+		int[] array2 = new int[] { 1, 2, 6 };
+		int[] array3 = new int[] { 0, 4, 5, 6 };
+
+		System.out.println(SmallestCommonNumber24.commonNumber(array1, array2, array3));
+
+	}
+}
--- src/searching/searchRotatedArray23.java
+++ src/searching/searchRotatedArray23.java
@@ -9,6 +9,32 @@ public class searchRotatedArray23 {
 
 	public static int shiftedBinarySearch(int[] array, int target, int left, int right) {
 
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			int potential = array[middle];
+			int leftNum = array[left];
+			int rightNum = array[right];
+
+			if (potential == target) {
+				return middle;
+			} else if (leftNum <= potential) {
+				if (leftNum <= target && potential > target) {
+					right = middle - 1;
+
+				} else {
+					left = middle + 1;
+				}
+
+			} else {
+				if (rightNum >= target && potential < target) {
+					left = middle + 1;
+
+				} else {
+					right = middle - 1;
+				}
+			}
+
+		}
 		return -1;
 	}
 
