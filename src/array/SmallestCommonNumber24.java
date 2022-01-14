b0edb99fa675a39f48ce1aec3261505d75c91f55
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-01-14 09:38:36 +0500

    update

--- src/array/SmallestCommonNumber24.java
+++ src/array/SmallestCommonNumber24.java
@@ -4,6 +4,19 @@ public class SmallestCommonNumber24 {
 
 	public static int commonNumber(int[] array1, int[] array2, int[] array3) {
 
+		int i = 0, j = 0, k = 0;
+		while (i < array1.length && j < array2.length && k < array3.length) {
+
+			if (array1[i] == array2[j] && array2[j] == array3[k]) {
+				return array1[i];
+			} else if (array1[i] < array2[j] && array1[i] < array3[k]) {
+				i++;
+			} else if (array2[j] < array3[k] && array2[j] < array1[i]) {
+				j++;
+			} else if (array3[k] < array2[j] && array3[k] < array1[i]) {
+				k++;
+			}
+		}
 		return -1;
 	}
 
