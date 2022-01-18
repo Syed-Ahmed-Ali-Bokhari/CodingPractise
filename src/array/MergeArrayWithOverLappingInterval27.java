c2cf494e4b086dc46036fed1bc2feb0a738fd398
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-01-18 10:48:15 +0500

    updated 27

--- src/array/MergeArrayWithOverLappingInterval27.java
+++ src/array/MergeArrayWithOverLappingInterval27.java
@@ -1,4 +1,5 @@
 package array;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,27 +9,53 @@ import java.util.Map;
 
 public class MergeArrayWithOverLappingInterval27 {
 
-	class Pair{
+	static public class Pair {
 		int first;
 		int second;
-		Pair(int x, int y)
-		{
-			this.first=x;
-			this.second=y;
+
+		Pair(int x, int y) {
+			this.first = x;
+			this.second = y;
 		}
 	}
-	public static ArrayList<Pair> mergeIntervals(ArrayList<Pair> v) {		// TODO Auto-generated method stub
-		
-		return null;
+
+	public static ArrayList<Pair> mergeIntervals(ArrayList<Pair> v) { // TODO Auto-generated method stub
+		ArrayList<Pair> result = new ArrayList<Pair>();
+		result.add(new Pair(v.get(0).first, v.get(0).second));
+		for (int i = 1; i < v.size(); i++) {
+			int x1=v.get(i).first;
+			int x2=v.get(i).second;
+			int y1=result.get(result.size()-1).first;
+			int y2=result.get(result.size()-1).second;
+			
+			if(y2>=x1)
+			{
+				result.get(result.size()-1).second=Math.max(x2 ,y2);
+			}
+			else
+				result.add(new Pair(x1,x2));
+					
+
+		}
+
+		return result;
 	}
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<Pair> v = new ArrayList<Pair>();
+		v.add(new Pair(1, 5));
+		v.add(new Pair(3, 7));
+		v.add(new Pair(4, 6));
+		v.add(new Pair(6, 8));
+
 		ArrayList<Pair> result = MergeArrayWithOverLappingInterval27.mergeIntervals(v);
 		
+		for(Pair p:result) {
+			System.out.println(p.first);
+			System.out.println(p.second);
+		}
+		
 	}
-
-	
 
 }
