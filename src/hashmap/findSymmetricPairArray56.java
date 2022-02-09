bd531df2e83233c4beaa9d77a70d3466c31e29c4
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-02-09 18:29:43 +0500

    updated 56 also

--- src/hashmap/findSymmetricPairArray56.java
+++ src/hashmap/findSymmetricPairArray56.java
@@ -1,34 +1,39 @@
 package hashmap;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class findSymmetricPairArray56 {
 
 	// Reverse words in a String
 
-	public static boolean subset(int[] array, int[] array1) {
-		Set<Integer> hset = new HashSet<Integer>();
+	public static String symmetric(int[][] array) {
+		Map<Integer,Integer> hmap = new HashMap<Integer,Integer>();
+		String result="";
 
 		for (int i = 0; i < array.length; i++) {
-			if(!hset.contains(array[i]))
-				hset.add(array[i]);
-		}
-		
-		for (int i = 0; i < array1.length; i++) {
-			if(hset.contains(array1[i]))
-				return false;
+			
+			Integer first = array[i][0];
+			Integer second = array[i][1];
+			Integer value = hmap.get(second);
+			if( value == first && value!=null)
+			{
+				result += "{"+ String.valueOf(first)+","+ String.valueOf(second)+"}";
+			}
+			else
+				hmap.put(first, second);
+			
 		}
 
-		return true;
+		return result;
 	}
 
 	public static void main(String[] args) {
 
-		int[] array = new int[] { 5, 1, 4, 2 };
-		int[] array1 = new int[] { 8, 10 };
-
-		boolean result = findSymmetricPairArray56.subset(array, array1);
+		int[][] array = new int[][] { { 1, 2 }, { 3, 4 }, { 5, 9 }, { 4, 3 } };
+		String result = findSymmetricPairArray56.symmetric(array);
 		System.out.println(result);
 
 	}
