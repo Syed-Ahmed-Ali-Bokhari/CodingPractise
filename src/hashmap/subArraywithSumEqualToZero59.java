24bf3875f297757005686831d450233babbce2f8
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-02-10 12:01:17 +0500

    updated 59 and 60

---
+++ src/hashmap/subArraywithSumEqualToZero59.java
@@ -0,0 +1,26 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class subArraywithSumEqualToZero59 {
+
+	// Reverse words in a String
+
+	public static boolean subArrayWithZero(int[] array) {
+		
+		
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] array = new int[] { 6, 4, -7, 3, 12, 9 };
+
+		Boolean result = subArraywithSumEqualToZero59.subArrayWithZero(array);
+		System.out.println(result);
+
+	}
+
+}
---
+++ src/hashmap/traceCompletePath60.java
@@ -0,0 +1,26 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class traceCompletePath60 {
+
+	// Reverse words in a String
+
+	public static String trace(int[] array) {
+		Map<String, String> hmap = new HashMap<String, String>();
+		
+		return "";
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] array = new int[] { 6, 4, -7, 3, 12, 9 };
+
+		String result = traceCompletePath60.trace(array);
+		System.out.println(result);
+
+	}
+
+}
