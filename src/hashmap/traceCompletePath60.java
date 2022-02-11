7c527ad06cb491940319b0166f40904459493fed
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-02-11 13:02:28 +0500

    updated 59

--- src/hashmap/traceCompletePath60.java
+++ src/hashmap/traceCompletePath60.java
@@ -5,20 +5,54 @@ import java.util.Map;
 
 public class traceCompletePath60 {
 
-	// Reverse words in a String
 
-	public static String trace(int[] array) {
-		Map<String, String> hmap = new HashMap<String, String>();
+	public static String trace(Map<String, String> map ) {
+		Map<String, String> reverse = new HashMap<String, String>();
 		
-		return "";
+		for(Map.Entry<String, String> entry : map.entrySet())
+		{
+			reverse.put(entry.getValue(), entry.getKey());
+		}
+		String from ="";
+		int count = 0;
+		for(Map.Entry<String, String> entry : map.entrySet())
+		{
+			if(!reverse.containsKey(entry.getKey())) {
+				count++;
+				from=entry.getKey();
+			}
+					
+		}
+		if(count>1)
+			return null;
+		
+		String result ="";
+		String to = map.get(from);
+		
+		while (to!=null)
+		{
+			 result += from +"->" + to + ",";
+			 from = to;
+			 to = map.get(from);
+			 
+			
+		}
+		
+		
+		return result;
 
 	}
 
 	public static void main(String[] args) {
 
-		int[] array = new int[] { 6, 4, -7, 3, 12, 9 };
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("NewYork", "chicago");
+		map.put("Bostan", "texas");
+		map.put("missouri", "NewYork");
+		map.put("texas", "missouri");
+	
 
-		String result = traceCompletePath60.trace(array);
+		String result = traceCompletePath60.trace(map);
 		System.out.println(result);
 
 	}
