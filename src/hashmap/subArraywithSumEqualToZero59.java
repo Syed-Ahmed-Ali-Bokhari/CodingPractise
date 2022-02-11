9745cf847a0469162f388080cb7ea262b3c30eb4
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-02-11 10:25:29 +0500

    updated 59

--- src/hashmap/subArraywithSumEqualToZero59.java
+++ src/hashmap/subArraywithSumEqualToZero59.java
@@ -8,8 +8,16 @@ public class subArraywithSumEqualToZero59 {
 	// Reverse words in a String
 
 	public static boolean subArrayWithZero(int[] array) {
-		
-		
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum+=array[i];
+			if(map.get(sum)!=null || sum ==0 || array[i]==0)
+				return true;
+			
+			map.put(sum, i);
+		}
+
 		return false;
 
 	}
