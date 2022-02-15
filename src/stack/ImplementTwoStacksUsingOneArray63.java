b7792a3f619923ccb18df6a62291e8cf5c40f151
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-02-16 01:07:33 +0500

    updated 63 and 64

--- src/stack/ImplementTwoStacksUsingOneArray63.java
+++ src/stack/ImplementTwoStacksUsingOneArray63.java
@@ -3,23 +3,47 @@ package stack;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class ImplementTwoStacksUsingOneArray63 {
+public class ImplementTwoStacksUsingOneArray63<V> {
 
-	// Reverse words in a String
+	// Logic is correct but no main method 
+	
+	int max;
+	int top1;
+	int top2;
+	V[] array;
 
-	public static String subset(int n) {
-	
-		return "";
-		}
+	@SuppressWarnings("unchecked")
+	public ImplementTwoStacksUsingOneArray63(int max) {
+		this.max = max;
+		top1 = -1;
+		top2 = max;
+		array = (V[]) new Object[max];
+	}
 
-	
-	
+	public void push1(V a) {
+		if (top1 < top2 - 1)
+			array[++top1] = a;
+	}
+
+	public void push2(V a) {
+		if (top2 > top1 + 1)
+			array[--top2] = a;
+	}
+
+	public V pop1() {
+		if (top1 > -1)
+			return array[top1--];
+		return null;
+	}
+
+	public V pop2() {
+		if (top2 < max)
+			return array[top2++];
+		return null;
+	}
 
 	public static void main(String[] args) {
-
-		int n = 3;
-		String result = ImplementTwoStacksUsingOneArray63.subset(n);
-
+		
 
 	}
 
--- src/stack/ReversingFirstKelementsOfQueue64.java
+++ src/stack/ReversingFirstKelementsOfQueue64.java
@@ -2,24 +2,29 @@ package stack;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.Stack;
 
 public class ReversingFirstKelementsOfQueue64 {
 
-	// Reverse words in a String
+	// Logic is correct but no main and improper syntaxes  
 
-	public static String subset(int n) {
-	
-		return "";
+	public static Queue reverse(int n) {
+		Stack stack = new Stack();
+		Queue q = new ArrayDeque();
+		for (int i = 0; i < n; i++) {
+			stack.push(q.remove());
 		}
+		while (!stack.isEmpty()) {
+			q.add(stack.pop());
+		}
+		for (int i = 0; i < n; i++) {
+			q.add(q.remove());
+		}
+		return q;
 
-	
-	
+	}
 
 	public static void main(String[] args) {
-
-		int n = 3;
-		String result = ReversingFirstKelementsOfQueue64.subset(n);
-
 
 	}
 
