85b535b09f7736917bbf33c260a2118fbda59b61
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-03-14 13:46:21 +0500

    completed 87

--- src/string/caeserCypherEncryption87.java
+++ src/string/caeserCypherEncryption87.java
@@ -1,38 +1,22 @@
 package string;
 
-import java.util.HashMap;
-
 class caeserCypherEncryption87 {
 
-	public static String prac(String str) {
-		String result = "";
-
+	public static String caeser(String str, int key) {
+		char[] chr  = new char[str.length()];
+		String alpha="abcdefghijklmnopqrstuvwxyz";
 		for (int i = 0; i < str.length(); i++) {
-			Character ch = str.charAt(i);
-			HashMap<Character, Integer> counting = new HashMap<Character, Integer>();
-			for (int j = i; j < str.length(); j++) {
-				if (ch == str.charAt(j)) {
-					if (counting.containsKey(ch)) {
-						counting.put(str.charAt(i), counting.get(str.charAt(i)) + 1);
-					} else {
-						counting.put(ch, 1);
-					}
-				}
-				else
-					break;
-				i=j;
-			}
-				result += ch.toString() + counting.get(ch).toString();
+			int value = ((str.charAt(i)+key)%97)%26;
+			chr[i]=alpha.charAt(value);
 		}
-		return result;
+		return new String(chr);
 	}
 
 // Driver code
 	public static void main(String[] args) {
-		String str = "aaawwwhhsjjjaa";
-		// a3w3h2s1j3a2
-		System.out.println(caeserCypherEncryption87.prac(str));
+		String str = "xyz";
+		int key = 4;
+		System.out.println(caeserCypherEncryption87.caeser(str, key));
+			
 	}
 }
-
-// This code is contributed by Satyam Singh
