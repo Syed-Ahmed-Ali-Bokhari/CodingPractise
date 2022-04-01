6504911ce9cd838a8760f0d8626ff2e12eaf2a99
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-04-01 18:19:00 +0500

    update till 97

---
+++ src/array/practise.java
@@ -0,0 +1,28 @@
+package array;
+
+public class practise {
+
+	// Reverse words in a String
+
+	public static int[] arrayOfProducts() {
+
+		for (int i = 13 ; i <= 27;i++) {
+			
+			
+		}
+		return null;
+		
+	}
+
+	public static void main(String[] args) {
+
+
+		
+
+		int[] result = practise.arrayOfProducts();
+		for (int res : result)
+			System.out.println(res);
+
+	}
+
+}
--- src/array/spiralTraverse97.java
+++ src/array/spiralTraverse97.java
@@ -1,16 +1,51 @@
 package array;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class spiralTraverse97 {
 
 	public static List<Integer> spiralTraverse(int[][] array) {
-		return null;
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		Integer startRow = 0;
+		Integer startCol = 0;
+		Integer endCol = array[0].length - 1;
+		Integer endRow = array.length - 1;
+		while (startRow <= endRow && startCol <= endCol) {
+			for (int col = startCol; col <= endCol; col++) {
+				result.add(array[startRow][col]);
+			}
+			for (int row = startRow + 1; row <= endRow; row++) {
+				result.add(array[row][endCol]);
+			}
+			for (int col = endCol - 1; col >= startCol; col--) {
 
+				if (startRow == endRow)
+					break;
+				result.add(array[endRow][col]);
+			}
+			for (int row = endRow - 1; row > startRow; row--) {
+
+				if (startCol == endCol)
+					break;
+				result.add(array[row][startCol]);
+			}
+
+			startRow++;
+			startCol++;
+			endRow--;
+			endCol--;
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
 
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		List<Integer> result = spiralTraverse97.spiralTraverse(arr);
+		for(int i=0;i<result.size();i++){
+		    System.out.println(result.get(i));
+		} 
 	}
 
 }
