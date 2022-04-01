f0b59b44c338087ad112ad883a311cd20aa22667
Ahmed Bokhari <ahmed.bokhari@seamless.se>
2022-04-02 00:52:28 +0500

    updated 98

--- src/array/longestPeak98.java
+++ src/array/longestPeak98.java
@@ -3,7 +3,29 @@ package array;
 public class longestPeak98 {
 
 	public static int longestPeak(int[] array) {
-		return 0;
+		int i = 1;
+		int longestPeak = 0;
+		while (i < array.length - 1) {
+
+			boolean flag = array[i - 1] < array[i] && array[i + 1] < array[i];
+			if (!flag) {
+				i++;
+				continue;
+			}
+			int left = i - 2;
+			while (left >= 0 && array[left] < array[left + 1]) {
+				left--;
+			}
+			int right = i + 2;
+			while (right < array.length && array[right] < array[right - 1]) {
+				right++;
+			}
+			int current = right - left - 1;
+			longestPeak = Math.max(longestPeak, current);
+
+			i = right;
+		}
+		return longestPeak;
 
 	}
 
