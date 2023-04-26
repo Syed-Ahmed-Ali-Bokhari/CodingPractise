009403fbb002fdf9a6f9a94ec48862dd741993fe
Syed-Ahmed-Ali-Bokhari <syedalyy1992@gmail.com>
2023-04-26 12:08:15 +0500

    updated

--- src/array/longestPeak98.java
+++ src/array/longestPeak98.java
@@ -1,6 +1,14 @@
 package array;
 
 public class longestPeak98 {
+// write a function that takes an array and return the length of the
+// longest peak in the array [1,2,3,3,4,0,10,6,5,-1,-3,2,3] ==> 6 (0,10,6,5,-1,-3)
+//A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip (the highest value
+//in the peak), at which point they become strictly decreasing. At least three integers are required to form a peak.
+// for example the integers 1,4,10,2 forms a peak but 4,0,10 and 1,2,2,0 and 1,2,3 does not form the peak
+//For example, the integers 1, 4, 10, 2 form a peak, but the integers 4, 0, 10 don't and neither do the integers
+//			1, 2, 2, 0. Similarly, the integers 1, 2, 3 don't form a peak because there aren't any strictly decreasing
+//	integers after the 3
 
 	public static int longestPeak(int[] array) {
 		int i = 1;
