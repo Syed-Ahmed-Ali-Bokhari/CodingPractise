20332e78954c848b3385657166d416d5a12a8722
Syed-Ahmed-Ali-Bokhari <syedalyy1992@gmail.com>
2023-05-01 17:06:59 +0500

    added new code

--- src/array/MaxSlidingWindow22.java
+++ src/array/MaxSlidingWindow22.java
@@ -5,34 +5,16 @@ import java.util.Deque;
 
 public class MaxSlidingWindow22 {
 
-	public static Deque<Integer> maxSliding(int[] array, int windowSize) {
+	public static int[] maxSliding(int[] array, int windowSize) {
+		int[] result = new int[array.length - windowSize + 1];
 
-		Deque<Integer> result = new ArrayDeque<Integer>();
-		Deque<Integer> list = new ArrayDeque<Integer>();
-
-		if (array.length < windowSize)
-			return result;
-
-		for (int i = 0; i < windowSize; i++) {
-			while (!list.isEmpty() && array[i] >= array[list.peekLast()]) {
-				list.removeLast();
+		for (int i = 0; i <= array.length - windowSize; i++) {
+			int maxInWindow = Integer.MIN_VALUE;
+			for (int j = i; j < i + windowSize; j++) {
+				maxInWindow = Math.max(maxInWindow, array[j]);
 			}
-			list.addLast(i);
+			result[i] = maxInWindow;
 		}
-		for (int i = windowSize; i < array.length; i++) {
-			result.add(array[list.peek()]);
-
-			while (!list.isEmpty() && array[list.peek()] >= i - windowSize) {
-				list.removeFirst();
-			}
-			
-			while (!list.isEmpty() && array[i] >= array[list.peekLast()]) {
-				list.removeLast();
-			}
-			list.addLast(i);
-		}
-
-		result.add(array[list.peek()]);
 
 		return result;
 	}
@@ -42,7 +24,7 @@ public class MaxSlidingWindow22 {
 		int[] array = new int[] { -4, 2, -5, 3, 6 };
 		int windowSize = 3;
 
-		Deque<Integer> result = MaxSlidingWindow22.maxSliding(array, windowSize);
+		int[] result = MaxSlidingWindow22.maxSliding(array, windowSize);
 		for (int res : result)
 			System.out.println(res);
 
--- src/array/practise.java
+++ src/array/practise.java
@@ -1,6 +1,7 @@
 package array;
 
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,24 +9,28 @@ public class practise {
 
     // Reverse words in a String
 
-    public static int firstDuplicateValue(int[] array) {
-        Set<Integer> set = new HashSet<Integer>();
-        for (int i: array) {
-            if (set.contains(i))
-                return i;
-            set.add(i);
+    public static int[] maxSliding(int[] array, int windowSize) {
+        int[] result = new int[array.length - windowSize + 1];
 
+        for (int i = 0; i <= array.length - windowSize; i++) {
+            int maxInWindow = Integer.MIN_VALUE;
+            for (int j = i; j < i + windowSize; j++) {
+                maxInWindow = Math.max(maxInWindow, array[j]);
+            }
+            result[i] = maxInWindow;
         }
 
-        return -1;
+        return result;
     }
+
     public static void main(String[] args) {
 
-        int[] array = new int[]{2,1,5,1,3,3,4};
+        int[] array = new int[]{-4, 2, -5, 3, 6};
+        int windowSize = 3;
 
-        int result = practise.firstDuplicateValue(array);
-        System.out.println(result);
-
+        int[] result = practise.maxSliding(array, windowSize);
+        for (int res : result)
+            System.out.println(res);
     }
 
 }
