bb3482e15a6c331fd8126c51b113fca3073d60e5
Syed-Ahmed-Ali-Bokhari <syedalyy1992@gmail.com>
2023-05-08 12:22:11 +0500

    updated

--- src/array/ValidateSequence3.java
+++ src/array/ValidateSequence3.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ValidateSequence3 {
-//	Validate Subsequence O T
+//	Validate Subsequence
 //	Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence
 //	of the first one.
 //	A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same
@@ -12,13 +12,9 @@ public class ValidateSequence3 {
 //[1, 2, 3, 4] , and so do the numbers [2, 4] . Note that a single number in an array and the array itself are
 //	both valid subsequences of the array.
 //	Sample Input
-//	array = 15, 1, 22, 25, 6,
-//			-1, 8, 10]
-//	sequence
-//=11, 6,
-//		-1, 10]
-//	Sample Output
-//true
+//	array = 15, 1, 22, 25, 6,-1, 8, 10]
+//	sequence = [1, 6,-1, 10]
+//	Sample Output ==> true
 	 public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
 	 		int i=0;
 			int j=0;
--- src/array/practise.java
+++ src/array/practise.java
@@ -7,20 +7,27 @@ public class practise {
 
     public static void main(String[] args) {
 
-        List<Integer> array =   new ArrayList<Integer>();
+        List<Integer> array = new ArrayList<Integer>();
         array.add(1);
         array.add(3);
         array.add(5);
-        List<Integer> sequence =   new ArrayList<Integer>();
+        List<Integer> sequence = new ArrayList<Integer>();
         sequence.add(1);
         sequence.add(3);
 
         System.out.println(practise.isValidSubsequence(array, sequence));
     }
 
-    private static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    private static boolean isValidSubsequence(List<Integer> list, List<Integer> sequence) {
+        int i = 0;
+        int j = 0;
+        while (i < list.size() && j < sequence.size()) {
+            if (list.get(i).equals(sequence.get(j)))
+                j++;
+            i++;
+        }
 
-        return false;
+        return j == sequence.size();
     }
 
 
