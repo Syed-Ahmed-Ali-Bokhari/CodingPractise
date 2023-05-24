08fcfe79e1002c4534b43218aa7201d734cc2a50
Syed-Ahmed-Ali-Bokhari <syedalyy1992@gmail.com>
2023-05-24 12:54:28 +0500

    pushed

--- src/string/practise.java
+++ src/string/practise.java
@@ -10,17 +10,31 @@ class practice {
     // Driver code
     public static void main(String[] args) {
         String characters = "bstes!hetsi ogEAxpelrt x ";
-        String document = "AlgoExpert is the best!";
+        String document = "AlgoExpert is the best!z";
 
         System.out.println(practice.generateDocument(document, characters));
 
     }
 
     private static boolean generateDocument(String document, String characters) {
+        Map<Character, Integer> mdoucment = new HashMap<Character, Integer>();
+        Map<Character, Integer> mcharacters = new HashMap<Character, Integer>();
 
 
+        for (char w : characters.toCharArray()) {
+            mcharacters.put(w, mcharacters.getOrDefault(w, 0) + 1);
+        }
+        for (char w : document.toCharArray()) {
+            mdoucment.put(w, mdoucment.getOrDefault(w, 0) + 1);
+            if (!mcharacters.containsKey(w)||mcharacters.get(w) == 0) {
+                return false;
+            }
+            mcharacters.put(w,mcharacters.get(w) - 1);
+        }
 
-        return false;
+        return true;
     }
+
 }
+
 
